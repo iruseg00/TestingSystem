4f01954ad73dbdbe91926170ba3f3211f9ebca53
Billerens <degtyarikilya@yandex.ru>
2021-01-23 14:23:21 +0300

    some little addings: icons, underlining, etc

--- app/src/components/menu/Menu.jsx
+++ app/src/components/menu/Menu.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import style from "./style.module.scss";
 import pc from "../../assets/images/pc.svg";
+import arrow from "../../assets/images/arrow.svg";
+import ellipse from "../../assets/images/ellipse.svg";
 
 class Menu extends React.Component {
   render() {
@@ -12,17 +14,22 @@ class Menu extends React.Component {
         <div className={style.items}>
           <div className={style.topItems}>
             <a className={style.menuItem} href="/dashboard/test-choosing">
+              <img className={style.ellipse} src={ellipse} alt="ellipse" />
               Выбор теста
             </a>
             <a className={style.menuItem} href="/dashboard/personal">
+              <img className={style.ellipse} src={ellipse} alt="ellipse" />
               Личный кабинет
             </a>
             <a className={style.menuItem} href="/dashboard/tests-passed">
+              <img className={style.ellipse} src={ellipse} alt="ellipse" />
               Пройденные тесты
             </a>
           </div>
           <a className={style.menuItem + " " + style.exit} href="/exit">
-            Выход из аккаунта
+            <img className={style.ellipse} src={ellipse} alt="ellipse" />
+            Выход из аккаунта{" "}
+            <img className={style.arrow} src={arrow} alt="arrow" />
           </a>
         </div>
       </div>
