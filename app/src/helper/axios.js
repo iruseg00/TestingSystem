cf9799d1d4fe2b3de581ee851f4b6862c0979f61
iruseg00 <isiruseg@gmail.com>
2021-01-24 16:19:36 +0300

    fixed server port to request

--- app/src/helper/axios.js
+++ app/src/helper/axios.js
@@ -4,7 +4,7 @@ import { logout } from "../redux/actions/auth";
 
 process.env.REACT_APP_ENV === "production"
   ? axios.defaults.baseURL = "http://185.66.71.54:8001/api/"
-  : axios.defaults.baseURL = "http://localhost:8000/api/";
+  : axios.defaults.baseURL = "http://localhost:33133/api/";
 
 axios.interceptors.response.use(
   response => {
@@ -12,6 +12,7 @@ axios.interceptors.response.use(
   },
   error => {
     if (error.response.status === 401) {
+      console.log(error)
       store.dispatch(logout());
     }
     return Promise.reject(error);
