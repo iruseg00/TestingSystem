b64605570ba2f7cdd877aae334e22e731878d519
Vitaly <antisvinnn@gmail.com>
2021-04-01 15:50:11 +0300

    #1_implemented

--- app/src/App.js
+++ app/src/App.js
@@ -1,71 +1,81 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import Register from "./views/Register/Register";
-import Main from "./views/Main/Main";
-import Login from "./views/Login/Login";
-import MainTests from "./views/dashboard/Dashboard";
-import PageWrapper from "./containers/pageWrapper/PageWrapper";
-import { logout } from "./redux/actions/auth";
-import { whoAmI } from "./redux/actions/users";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import Register from './views/register/Register';
+import Main from './views/main/Main';
+import Login from './views/login/Login';
+import MainTests from './views/dashboard/Dashboard';
+import PageWrapper from './containers/pageWrapper/PageWrapper';
+import { logout } from './redux/actions/auth';
+import { whoAmI } from './redux/actions/users';
 
 class App extends React.Component {
-  componentDidMount() {
-    if (this.props.auth.accessToken)
-      this.props.whoAmI().catch(() => {
-        this.props.logout();
-      });
-  }
+	componentDidMount() {
+		if (this.props.auth.accessToken)
+			this.props.whoAmI().catch(() => {
+				this.props.logout();
+			});
+	}
 
-  render() {
-    return (
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={props => (
-            <PageWrapper {...props} title="Главная" component={Main} Auth />
-          )}
-        />
-        <Route
-          path="/login"
-          render={(props) => (
-            <PageWrapper {...props} title="Вход" component={Login} notAuth />
-          )}
-        />
-        <Route
-          path="/register"
-          render={(props) => (
-            <PageWrapper
-              {...props}
-              title="Регистрация"
-              component={Register}
-              notAuth
-            />
-          )}
-        />
-        <Route
-          path="/dashboard"
-          render={(props) => (
-            <PageWrapper {...props} component={MainTests} />
-          )}
-        />
-        <Route
-          path="*"
-          exact
-          render={(props) => (
-            <PageWrapper {...props} title="Упс!" component={Login} />
-          )}
-        />
-      </Switch>
-    );
-  }
+	render() {
+		return (
+			<Switch>
+				<Route
+					exact
+					path='/'
+					render={(props) => (
+						<PageWrapper
+							{...props}
+							title='Главная'
+							component={Main}
+							Auth
+						/>
+					)}
+				/>
+				<Route
+					path='/login'
+					render={(props) => (
+						<PageWrapper
+							{...props}
+							title='Вход'
+							component={Login}
+							notAuth
+						/>
+					)}
+				/>
+				<Route
+					path='/register'
+					render={(props) => (
+						<PageWrapper
+							{...props}
+							title='Регистрация'
+							component={Register}
+							notAuth
+						/>
+					)}
+				/>
+				<Route
+					path='/dashboard'
+					render={(props) => (
+						<PageWrapper {...props} component={MainTests} />
+					)}
+				/>
+				<Route
+					path='*'
+					exact
+					render={(props) => (
+						<PageWrapper {...props} title='Упс!' component={Login} />
+					)}
+				/>
+			</Switch>
+		);
+	}
 }
 
 const mapStateToProps = ({ auth }) => ({ auth });
 const mapDispatchToProps = {
-  logout,
-  whoAmI,
+	logout,
+	whoAmI,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
