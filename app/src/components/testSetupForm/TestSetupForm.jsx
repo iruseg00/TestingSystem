5f12c73da13513ea1d40ee7ceed63031c551379b
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-04-04 15:00:23 +0300

    stepps checkpoint

--- app/src/components/testSetupForm/TestSetupForm.jsx
+++ app/src/components/testSetupForm/TestSetupForm.jsx
@@ -1,53 +1,53 @@
-import { Form, Input, Button } from "antd";
-import style from "./style.module.scss";
+import { Form, Input, Button } from 'antd';
+import style from './style.module.scss';
 
 function TestSetupForm() {
-  return (
-    <Form
-      className={style.form}
-      name="testSetup"
-      // onFinish={onFinish}
-      // onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        className={style.wrapper_input}
-        label="Присвоенный ID"
-        name="testID"
-        rules={[
-          {
-            required: false,
-            message: "Please input test ID or generate it!",
-          },
-        ]}
-      >
-        <Input size="large" className={style.input} />
-      </Form.Item>
-      <Form.Item
-        className={style.wrapper_input}
-        label="Тестируемая система"
-        name="system"
-        rules={[
-          {
-            required: false,
-            message: "Please input tested system name!",
-          },
-        ]}
-      >
-        <Input size="large" className={style.input} />
-      </Form.Item>
-      <Form.Item className={style.wrapper_input} label="Примечание" name="note">
-        <Input size="large" className={style.input} />
-      </Form.Item>
-      <a className={style.scanningText} href={"/test-scanning"}>
-        Сканировать имеющийся тест
-      </a>
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className={style.submit}>
-          Начать тест
-        </Button>
-      </Form.Item>
-    </Form>
-  );
+	return (
+		<Form
+			className={style.form}
+			name='testSetup'
+			// onFinish={onFinish}
+			// onFinishFailed={onFinishFailed}
+		>
+			<Form.Item
+				className={style.wrapper_input}
+				label='Присвоенный ID'
+				name='testID'
+				rules={[
+					{
+						required: false,
+						message: 'Please input test ID or generate it!',
+					},
+				]}
+			>
+				<Input size='large' className={style.input} />
+			</Form.Item>
+			<Form.Item
+				className={style.wrapper_input}
+				label='Тестируемая система'
+				name='system'
+				rules={[
+					{
+						required: false,
+						message: 'Please input tested system name!',
+					},
+				]}
+			>
+				<Input size='large' className={style.input} />
+			</Form.Item>
+			<Form.Item className={style.wrapper_input} label='Примечание' name='note'>
+				<Input size='large' className={style.input} />
+			</Form.Item>
+			<a className={style.scanningText} href={'/test-scanning'}>
+				Сканировать имеющийся тест
+			</a>
+			<Form.Item>
+				<Button type='primary' htmlType='submit' className={style.submit}>
+					Начать тест
+				</Button>
+			</Form.Item>
+		</Form>
+	);
 }
 
 export default TestSetupForm;
--- app/src/views/dashboard/susTestSetup/SUSTestSetup.jsx
+++ app/src/views/dashboard/susTestSetup/SUSTestSetup.jsx
@@ -1,17 +1,23 @@
-import style from "./style.module.scss";
-import TestSetup from "../../../components/testSetupForm/TestSetupForm";
-import boxes from "../../../assets/images/cubes-solid_1.svg";
+import style from './style.module.scss';
+import TestSetup from '../../../components/testSetupForm/TestSetupForm';
+import boxes from '../../../assets/images/cubes-solid_1.svg';
+import Stepps from './../../../components/steps/Steps';
 
 function SUSTest() {
-  return (
-    <div className={style.container}>
-      <img className={style.boxes} src={boxes} alt="boxes" />
-      <div className={style.testSetups}>
-        <div className={style.testName}>System Usability Scale</div>
-        <TestSetup />
-      </div>
-    </div>
-  );
+	return (
+		<div className={style.container}>
+			<div className={style.container_to_steps}>
+				<Stepps step_one={TestSetup} />
+			</div>
+			<div className={style.continer_to_test_init}>
+				<img className={style.boxes} src={boxes} alt='boxes' />
+				<div className={style.testSetups}>
+					<div className={style.testName}>System Usability Scale</div>
+					<TestSetup />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default SUSTest;
