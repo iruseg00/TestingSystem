4b569cb638fabb82254974ab211dbed76768e5b0
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-04-04 03:18:00 +0300

    sideBar_fixedVersion

--- app/src/components/header/Header.jsx
+++ app/src/components/header/Header.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { Avatar, Drawer } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { logout } from '../../redux/actions/auth';
 import style from './style.module.scss';
 import barsImage from '../../assets/images/bars-solid_2.svg';
-import { Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-import { useState } from 'react';
-import { Drawer } from 'antd';
-import { Link } from 'react-router-dom';
-import PC from './../../assets/images/pc.svg';
+import PC from './../../assets/images/ii2.jpg';
 
 const Header = (props) => {
 	const [visible, setVisible] = useState(false);
-
+	const dispatch = useDispatch();
 	const showDrawer = () => {
 		setVisible(true);
 	};
@@ -24,41 +24,42 @@ const Header = (props) => {
 			<Drawer
 				className={style.menu}
 				placement='left'
-				width='500'
+				width='330'
 				closable={false}
 				onClose={onClose}
 				visible={visible}
 			>
 				<div className={style.links_container}>
-					<Link to='/dashboard'>
-						<div className={style.side_bar_item}>Выбор теста</div>
-					</Link>
+					<div className={style.flex_1}>
+						<Link className={style.side_bar_item} to='/dashboard'>
+							Выбор теста
+						</Link>
+						<Link className={style.side_bar_item} to='/dashboard/profile'>
+							Личный кабинет
+						</Link>
+						<Link className={style.side_bar_item} to='/dashboard/passed_tests'>
+							Пройденные тесты
+						</Link>
+					</div>
 
-					<Link to='/dashboard/profile'>
-						<div className={style.side_bar_item}>Личный кабинет</div>
-					</Link>
-
-					<Link to='/dashboard/passed_tests'>
-						<div className={style.side_bar_item}>Пройденные тесты</div>
-					</Link>
 					<div className={style.container_to_PC}>
 						<img src={PC} alt='PC' />
 					</div>
-					<Link to='/login'>
-						<div className={style.side_bar_item}>Выйти из аккаунта</div>
+					<Link onClick={() => dispatch(logout)} to='/login'>
+						<div className={style.side_bar_item + ' ' + style.exit_link}>Выйти из аккаунта</div>
 					</Link>
 				</div>
 			</Drawer>
+
 			<div className={style.left_items}>
 				<div className={style.bar_img_container}>
 					<img onClick={showDrawer} className={style.barImg} src={barsImage} alt='barsImage' />
 				</div>
 				<div className={style.title}>Тестирование</div>
 			</div>
-			<div className={style.right_items}>
-				<div className={style.avatar_container}>
-					<Avatar size={64} icon={<UserOutlined />} />
-				</div>
+
+			<div className={style.avatar_container}>
+				<Avatar size={64} icon={<UserOutlined />} />
 			</div>
 		</div>
 	);
