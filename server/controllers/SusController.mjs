4bc869aa6a1ea89bc0e3f808b8cecb167afef80d
iruseg00 <isiruseg@gmail.com>
2021-04-10 02:22:26 +0300

    #17 implemented

--- server/controllers/SusController.mjs
+++ server/controllers/SusController.mjs
@@ -16,17 +16,22 @@ router.get("/all_answers", (req, res) => {
     .catch((err) => res.status(500).send(err));
 });
 
-router.post("/create_answer", (req, res) => {
-  const value = (
-    (req.body.answers.reduce((summa, item) => summa + item.answer, 0) /
-    req.body.answers.length) * 20).toFixed(0);
-  const type =
-    value >= 90 ? "A1" : value >= 60 ? "A2" : value >= 40 ? "A3" : "A4";
-  req.body.results = { value, percentile: 94, type };
-  req.body.user = req.user.id;
-  SusTestService.create(req.body)
-    .then((data) => res.status(201).json(data.results))
-    .catch((err) => res.status(400).send(err));
+router.post("/create_answer", async (req, res) => {
+  try {
+    const value = (
+      (req.body.answers.reduce((summa, item) => summa + item.answer, 0) /
+        req.body.answers.length) *
+      20
+    ).toFixed(0);
+    const type =
+      value >= 90 ? "A1" : value >= 60 ? "A2" : value >= 40 ? "A3" : "A4";
+    req.body.results = { value, percentile: 94, type };
+    req.body.user = req.user.id;
+    const data = await SusTestService.create(req.body);
+    res.status(201).json(data.results);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 export default router;
