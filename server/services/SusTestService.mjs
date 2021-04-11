5f39a424f548dd4f7a4c1357065647c197ae99c9
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-04-11 17:51:10 +0300

    ifx_checkpoint_6

--- server/services/SusTestService.mjs
+++ server/services/SusTestService.mjs
@@ -1,24 +1,12 @@
 import SusTest from '../db/models/SusTest.mjs';
 
-<<<<<<< HEAD
 class SusTestService {
-	getAll() {
+	getAll(id) {
 		return SusTest.findAll({
 			attributes: ['user', 'results', 'testingSystem', 'description'],
+			where: { id },
 		});
 	}
-=======
-class SusTestService 
-{
-    getAll(id)
-    {
-        return SusTest.findAll({
-          attributes: ["user", "results", "testingSystem", "description"],
-          where: { id },
-        });
-    }
->>>>>>> 6b6a1c72e7e1716ce35b9d10eda1af70b4537487
-
 	create(body) {
 		return SusTest.create({
 			user: body.user,
