45626bde8646da3bc15bf19eba3c07b2ef079159
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-04-14 06:26:27 +0300

    test-list-startPoint-2

--- app/src/views/passedTests/index.jsx
+++ app/src/views/passedTests/index.jsx
@@ -1,14 +1,29 @@
 import PassedTest from '../../components/passedTest/PassedTest';
 import style from './style.module.scss';
 import boxes from '../../assets/images/Vector.svg';
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 const PassedTests = () => {
 	return (
-		<div className={style.container}>
-			<PassedTest image={boxes} title={'SUS'} />
-			<PassedTest image={boxes} title={'PSSUQ'} />
-			<PassedTest image={boxes} title={'MDT'} />
-			<PassedTest image={boxes} title={'SUS'} />
+		<div className={style.general}>
+			<div className={style.container}>
+				<PassedTest image={boxes} title={'SUS'} />
+				<PassedTest image={boxes} title={'PSSUQ'} />
+				<PassedTest image={boxes} title={'MDT'} />
+				<PassedTest image={boxes} title={'SUS'} />
+				<PassedTest image={boxes} title={'SUS'} />
+				<PassedTest image={boxes} title={'PSSUQ'} />
+				<PassedTest image={boxes} title={'MDT'} />
+				<PassedTest image={boxes} title={'SUS'} />
+			</div>
+			<div className={style.containerToButton}>
+				<Link to='/dashboard'>
+					<Button type='primary' htmlType='submit' className={style.submit}>
+						Подтвердить
+					</Button>
+				</Link>
+			</div>
 		</div>
 	);
 };
