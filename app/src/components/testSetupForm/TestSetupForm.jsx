df69d1b2258905b800b83ec7844b1a1b899dbfcc
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-04-17 00:34:53 +0300

    all_modals_checkpoint

--- app/src/components/testSetupForm/TestSetupForm.jsx
+++ app/src/components/testSetupForm/TestSetupForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Input } from 'antd';
+import { Button, Input, Modal } from 'antd';
 import style from './style.module.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import Form from 'antd/lib/form/Form';
@@ -17,6 +17,28 @@ function TestSetupForm(props) {
 		dispatch(props.getTest());
 		props.func_next();
 	};
+	// модальные хуки
+	const [visible, setVisible] = React.useState(false);
+	const [confirmLoading, setConfirmLoading] = React.useState(false);
+	const [modalText, setModalText] = React.useState(
+		'Вы уверены, что все данные введены верно?'
+	);
+	const showModal = () => {
+		setVisible(true);
+	};
+	const handleOk = () => {
+		setModalText('Идёт синхронизация с базой данных...');
+		setConfirmLoading(true);
+		setTimeout(() => {
+			setVisible(false);
+			setConfirmLoading(false);
+		}, 2000);
+	};
+	const handleCancel = () => {
+		console.log('Clicked cancel button');
+		setVisible(false);
+	};
+
 	return (
 		<Form onFinish={onFinish} className={style.form} name='testSetup'>
 			<div className={style.fields}>
@@ -61,9 +83,50 @@ function TestSetupForm(props) {
 			<Link className={style.scanningText} to='/test-scanning'>
 				Сканировать имеющийся тест
 			</Link>
-			<Button type='primary' htmlType='submit' className={style.submit}>
+			<Button type='primary' htmlType='submit' onClick={showModal} className={style.submit}>
 				Начать тест
 			</Button>
+			{/* МОДАЛКА */}
+			<Modal
+				centered
+				bodyStyle={{
+					border: '1px solid black',
+					borderRadius: '10px',
+					backgroundColor: '#e6e6e6',
+					height: '216px',
+					width: '527px',
+					padding: '40px',
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'space-between',
+				}}
+				className={style.modalWindow}
+				footer={null}
+				closable={false}
+				visible={visible}
+				confirmLoading={confirmLoading}
+			>
+				<p className={style.modalText}>{modalText}</p>
+				<div className={style.containerToModalButtons}>
+					<Button
+						name='modalSubmit'
+						type='default'
+						htmlType='submit'
+						className={style.submit_modal}
+						onClick={handleOk}
+					>
+						Да
+					</Button>
+					<Button
+						type='primary'
+						htmlType='reset'
+						className={style.cancel_modal}
+						onClick={handleCancel}
+					>
+						Нет
+					</Button>
+				</div>
+			</Modal>
 		</Form>
 	);
 }
