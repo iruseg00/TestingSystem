3649b40cde93b98f638b5224269defc4bfe9699e
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-04-17 00:00:12 +0300

    modal_window_register_without_logic

--- app/src/components/registerForma/RegisterForma.jsx
+++ app/src/components/registerForma/RegisterForma.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Form, Input, Button, Radio } from 'antd';
+import { Form, Input, Button, Radio, Modal } from 'antd';
 import { register } from '../../redux/actions/registration';
 
 import style from './style.module.scss';
@@ -53,6 +53,28 @@ function RegisterForma(props) {
 		}
 	};
 	const onChangeAvatar = (image) => (userPhoto = image || null);
+	// модальные хуки
+	const [visible, setVisible] = React.useState(false);
+	const [confirmLoading, setConfirmLoading] = React.useState(false);
+	const [modalText, setModalText] = React.useState(
+		'Вы уверены, что все данные введены верно?'
+	);
+	const showModal = () => {
+		setVisible(true);
+	};
+	const handleOk = () => {
+		setModalText('Идёт синхронизация с базой данных...');
+		setConfirmLoading(true);
+		setTimeout(() => {
+			setVisible(false);
+			setConfirmLoading(false);
+		}, 2000);
+	};
+	const handleCancel = () => {
+		console.log('Clicked cancel button');
+		setVisible(false);
+	};
+
 	return (
 		<Form
 			form={form}
@@ -144,12 +166,13 @@ function RegisterForma(props) {
 				>
 					<Input.Password size='large' className={style.input} onChange={changedField} />
 				</Form.Item>
+				{/* КНОПКА РЕГИСТРАЦИИ */}
 				<Form.Item>
 					<Button
 						type='primary'
-						htmlType='submit'
+						// htmlType='submit'
+						onClick={showModal}
 						className={style.submit}
-						loading={props.registration.loading}
 					>
 						Готово
 					</Button>
@@ -158,6 +181,48 @@ function RegisterForma(props) {
 			<Form.Item className={style.photo} name='photo'>
 				<DragnDropImage onChange={onChangeAvatar} />
 			</Form.Item>
+			{/* МОДАЛКА */}
+			<Modal
+				centered
+				bodyStyle={{
+					border: '1px solid black',
+					borderRadius: '10px',
+					backgroundColor: '#e6e6e6',
+					height: '216px',
+					width: '527px',
+					padding: '40px',
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'space-between',
+				}}
+				className={style.modalWindow}
+				footer={null}
+				closable={false}
+				visible={visible}
+				confirmLoading={confirmLoading}
+			>
+				<p className={style.modalText}>{modalText}</p>
+				<div className={style.containerToModalButtons}>
+					<Button
+						name='modalSubmit'
+						type='default'
+						htmlType='submit'
+						className={style.submit_modal}
+						loading={props.registration.loading}
+						onClick={handleOk}
+					>
+						Да
+					</Button>
+					<Button
+						type='primary'
+						htmlType='reset'
+						className={style.cancel_modal}
+						onClick={handleCancel}
+					>
+						Нет
+					</Button>
+				</div>
+			</Modal>
 		</Form>
 	);
 }
