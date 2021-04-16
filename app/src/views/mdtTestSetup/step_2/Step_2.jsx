8320960a2fe65f5d481f8de7134f57098589ca84
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-04-17 01:28:58 +0300

    all modals everywhere without logic

--- app/src/views/mdtTestSetup/step_2/Step_2.jsx
+++ app/src/views/mdtTestSetup/step_2/Step_2.jsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import style from './style.module.scss';
 import TestMdtQuestions from '../../../components/testMdtQuestion/TestMdtQuestion';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import Form from 'antd/lib/form/Form';
 import FormItem from 'antd/lib/form/FormItem';
 import { getResults } from '../../../redux/actions/mdtTest';
@@ -36,6 +37,27 @@ const Step_2 = (props) => {
 		dispatch(getResults(answer_to_post));
 		props.func_next();
 	};
+	// модальные хуки
+	const [visible, setVisible] = React.useState(false);
+	const [confirmLoading, setConfirmLoading] = React.useState(false);
+	const [modalText, setModalText] = React.useState(
+		'Вы уверены, что все данные введены верно?'
+	);
+	const showModal = () => {
+		setVisible(true);
+	};
+	const handleOk = () => {
+		setModalText('Идёт синхронизация с базой данных...');
+		setConfirmLoading(true);
+		setTimeout(() => {
+			setVisible(false);
+			setConfirmLoading(false);
+		}, 2000);
+	};
+	const handleCancel = () => {
+		console.log('Clicked cancel button');
+		setVisible(false);
+	};
 
 	return (
 		<Form name='Form' onFinish={PostAnswers}>
@@ -46,10 +68,56 @@ const Step_2 = (props) => {
 				</div>
 				<div className={style.content_container}>{arrayOfQuestions}</div>
 				<FormItem name='button'>
-					<Button type='primary' htmlType='submit' className={style.submit}>
+					<Button
+						type='primary'
+						// htmlType='submit'
+						onClick={showModal}
+						className={style.submit}
+					>
 						Подтвердить
 					</Button>
 				</FormItem>
+				{/* МОДАЛКА */}
+				<Modal
+					centered
+					bodyStyle={{
+						border: '1px solid black',
+						borderRadius: '10px',
+						backgroundColor: '#e6e6e6',
+						height: '216px',
+						width: '527px',
+						padding: '40px',
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'space-between',
+					}}
+					className={style.modalWindow}
+					footer={null}
+					closable={false}
+					visible={visible}
+					confirmLoading={confirmLoading}
+				>
+					<p className={style.modalText}>{modalText}</p>
+					<div className={style.containerToModalButtons}>
+						<Button
+							name='modalSubmit'
+							type='default'
+							htmlType='submit'
+							className={style.submit_modal}
+							onClick={handleOk}
+						>
+							Да
+						</Button>
+						<Button
+							type='primary'
+							htmlType='reset'
+							className={style.cancel_modal}
+							onClick={handleCancel}
+						>
+							Нет
+						</Button>
+					</div>
+				</Modal>
 			</div>
 		</Form>
 	);
--- app/src/views/pssuqTestSetup/step_2/Step_2.jsx
+++ app/src/views/pssuqTestSetup/step_2/Step_2.jsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import style from './style.module.scss';
 import TestPssuqQuestions from '../../../components/testPssuqQuestion/TestPssuqQuestion';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import Form from 'antd/lib/form/Form';
 import FormItem from 'antd/lib/form/FormItem';
 import { getResults } from '../../../redux/actions/pssuqTest';
@@ -35,6 +36,27 @@ const Step_2 = (props) => {
 		dispatch(getResults(answer_to_post));
 		props.func_next();
 	};
+	// модальные хуки
+	const [visible, setVisible] = React.useState(false);
+	const [confirmLoading, setConfirmLoading] = React.useState(false);
+	const [modalText, setModalText] = React.useState(
+		'Вы уверены, что все данные введены верно?'
+	);
+	const showModal = () => {
+		setVisible(true);
+	};
+	const handleOk = () => {
+		setModalText('Идёт синхронизация с базой данных...');
+		setConfirmLoading(true);
+		setTimeout(() => {
+			setVisible(false);
+			setConfirmLoading(false);
+		}, 2000);
+	};
+	const handleCancel = () => {
+		console.log('Clicked cancel button');
+		setVisible(false);
+	};
 
 	return (
 		<Form name='Form' onFinish={PostAnswers}>
@@ -42,11 +64,57 @@ const Step_2 = (props) => {
 				<div className={style.title}>{props.title_of_test}</div>
 				<div className={style.content_container}>{arrayOfQuestions}</div>
 				<FormItem name='button'>
-					<Button type='primary' htmlType='submit' className={style.submit}>
+					<Button
+						type='primary'
+						onClick={showModal}
+						htmlType='submit'
+						className={style.submit}
+					>
 						Подтвердить
 					</Button>
 				</FormItem>
 			</div>
+			{/* МОДАЛКА */}
+			<Modal
+				centered
+				bodyStyle={{
+					border: '1px solid black',
+					borderRadius: '10px',
+					backgroundColor: '#e6e6e6',
+					height: '216px',
+					width: '527px',
+					padding: '40px',
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'space-between',
+				}}
+				className={style.modalWindow}
+				footer={null}
+				closable={false}
+				visible={visible}
+				confirmLoading={confirmLoading}
+			>
+				<p className={style.modalText}>{modalText}</p>
+				<div className={style.containerToModalButtons}>
+					<Button
+						name='modalSubmit'
+						type='default'
+						htmlType='submit'
+						className={style.submit_modal}
+						onClick={handleOk}
+					>
+						Да
+					</Button>
+					<Button
+						type='primary'
+						htmlType='reset'
+						className={style.cancel_modal}
+						onClick={handleCancel}
+					>
+						Нет
+					</Button>
+				</div>
+			</Modal>
 		</Form>
 	);
 };
