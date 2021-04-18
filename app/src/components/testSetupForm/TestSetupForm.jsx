f052375389e979059b452f8806bafe7d39db736f
Vitaly Tochenyy <antisvinnn@gmail.com>
2021-04-18 23:35:16 +0300

    branch_1_Task_checkpoint_

--- app/src/components/testSetupForm/TestSetupForm.jsx
+++ app/src/components/testSetupForm/TestSetupForm.jsx
@@ -1,42 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Input, Modal } from 'antd';
+import { Button, Input } from 'antd';
 import style from './style.module.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import Form from 'antd/lib/form/Form';
 import FormItem from 'antd/lib/form/FormItem';
+import ModalWindow from '../modal/ModalWindow';
 
 function TestSetupForm(props) {
 	const USER_ID = useSelector((state) => state.users.profile.userID);
+	const [visible, setVisible] = React.useState(false);
+	const [action, setAction] = React.useState();
 	const dispatch = useDispatch();
 	const onFinish = (values) => {
-		props.setData({
-			testingSystem: values.testingSystem,
-			description: values.description,
-		});
-		dispatch(props.getTest());
-		props.func_next();
-	};
-	// модальные хуки
-	const [visible, setVisible] = React.useState(false);
-	const [confirmLoading, setConfirmLoading] = React.useState(false);
-	const [modalText, setModalText] = React.useState(
-		'Вы уверены, что все данные введены верно?'
-	);
-	const showModal = () => {
+		const send = () => () => {
+			props.setData({
+				testingSystem: values.testingSystem,
+				description: values.description,
+			});
+			dispatch(props.getTest());
+			props.func_next();
+		};
+		setAction(send);
 		setVisible(true);
-	};
-	const handleOk = () => {
-		setModalText('Идёт синхронизация с базой данных...');
-		setConfirmLoading(true);
-		setTimeout(() => {
-			setVisible(false);
-			setConfirmLoading(false);
-		}, 2000);
-	};
-	const handleCancel = () => {
-		console.log('Clicked cancel button');
-		setVisible(false);
 	};
 
 	return (
@@ -83,50 +69,12 @@ function TestSetupForm(props) {
 			<Link className={style.scanningText} to='/test-scanning'>
 				Сканировать имеющийся тест
 			</Link>
-			<Button type='primary' htmlType='submit' onClick={showModal} className={style.submit}>
+			<Button type='primary' htmlType='submit' className={style.submit}>
 				Начать тест
 			</Button>
-			{/* МОДАЛКА */}
-			<Modal
-				centered
-				bodyStyle={{
-					border: '1px solid black',
-					borderRadius: '10px',
-					backgroundColor: '#e6e6e6',
-					height: '216px',
-					width: '527px',
-					padding: '40px',
-					display: 'flex',
-					flexDirection: 'column',
-					justifyContent: 'space-between',
-				}}
-				className={style.modalWindow}
-				footer={null}
-				closable={false}
-				visible={visible}
-				confirmLoading={confirmLoading}
-			>
-				<p className={style.modalText}>{modalText}</p>
-				<div className={style.containerToModalButtons}>
-					<Button
-						name='modalSubmit'
-						type='default'
-						htmlType='submit'
-						className={style.submit_modal}
-						onClick={handleOk}
-					>
-						Да
-					</Button>
-					<Button
-						type='primary'
-						htmlType='reset'
-						className={style.cancel_modal}
-						onClick={handleCancel}
-					>
-						Нет
-					</Button>
-				</div>
-			</Modal>
+			<ModalWindow action={action} visible={visible} setVisible={setVisible}>
+				Вы уверены, что все данные введены верно?
+			</ModalWindow>
 		</Form>
 	);
 }
