a243de9f74f099698b09c3d3439b685729d534c9
iruseg00 <isiruseg@gmail.com>
2021-05-08 21:11:01 +0300

    corrected date

--- app/src/components/testingSystemResult/TestingSystemResult.jsx
+++ app/src/components/testingSystemResult/TestingSystemResult.jsx
@@ -2,15 +2,17 @@ import { Link } from 'react-router-dom';
 import style from './style.module.scss';
 
 const TestingSystemResult = ({ date, description, testingSystem, typeOfTest, ID }) => {
-	const DATE = date && new Date(date).toISOString().substring(0, 10);
-	const TIME = date && new Date(date).toISOString().substring(11, 16);
-	const newDate = DATE + ' ' + TIME;
+	date = date && new Date(date).setMilliseconds(10800000)
+	date = date &&
+		new Date(date).toISOString().substring(0, 10) + 
+		' ' +
+		new Date(date).toISOString().substring(11, 16);
 	return (
 		<Link to={`/dashboard/passed_tests/${typeOfTest.toLowerCase()}/${testingSystem}/${ID}`}>
 			<div className={style.container}>
 				<div className={style.typeOfTest}>{typeOfTest}</div>
 				<div className={style.description}>{description}</div>
-				<div className={style.date}>{newDate}</div>
+				<div className={style.date}>{date}</div>
 			</div>
 		</Link>
 	);
