b4f9aa3a1322fd9b73051362b9914f925c29f41b
iruseg00 <isiruseg@gmail.com>
2021-05-19 00:29:03 +0300

    AcTest

--- server/controllers/DcController.mjs
+++ server/controllers/DcController.mjs
@@ -1,13 +1,13 @@
 import express from "express";
 const router = express.Router();
 
-import PssuqTableService from "../services/PssuqTableService.mjs";
-import PssuqTestService from "../services/PssuqTestService.mjs";
-import { PssuqTest } from "../services/ComputationService.mjs";
+import DcTableService from "../services/DcTableService.mjs";
+import DcTestService from "../services/DcTestService.mjs";
+import { SusTest } from "../services/ComputationService.mjs"; // TODO
 
 router.get("/all_questions", async (req, res) => {
   try {
-    const data = await PssuqTableService.getAll();
+    const data = await DcTableService.getAll();
     res.status(200).json(data);
   } catch (error) {
     res.status(400).send(error);
@@ -16,19 +16,19 @@ router.get("/all_questions", async (req, res) => {
 
 router.get("/all_answers", async (req, res) => {
   try {
-    const data = await PssuqTestService.getAll(req.user.id);
+    const data = await DcTestService.getAll(req.user.id);
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).send(error);
+    res.status(400).send(error);
   }
 });
 
 router.post("/testing_system", async (req, res) => {
   try {
-    const data = await PssuqTestService.getAllByTestingSystem(
+    const data = await DcTestService.getAllByTestingSystem(
       req.body.testingSystem
     );
-    res.status(200).json(data.reverse());
+    return res.status(200).json(data.reverse());
   } catch (error) {
     res.status(400).send("Invalid data!");
   }
@@ -36,9 +36,10 @@ router.post("/testing_system", async (req, res) => {
 
 router.post("/create_answer", async (req, res) => {
   try {
-    req.body.results = await PssuqTest(req.body.answers);
+    const { value, type } = await SusTest(req.body.answers);
+    req.body.results = { value, percentile: 94, type };
     req.body.user = req.user.id;
-    const data = await PssuqTestService.create(req.body);
+    const data = await DcTestService.create(req.body);
     res.status(201).json(data.results);
   } catch (error) {
     res.status(400).send(error);
--- server/services/ComputationService.mjs
+++ server/services/ComputationService.mjs
@@ -1,33 +1,112 @@
+import AcKey from "../constants/acKey.json";
+
+// MULTIPLIER_PSSUQ_COMMON = 100 / 180 * 100 / 180;
+const MULTIPLIER_PSSUQ_COMMON = 0.308642;
+// MULTIPLIER_PSSUQ_USEFULNESS = 100 / 80;
+const MULTIPLIER_PSSUQ_USEFULNESS = 1.25;
+// MULTIPLIER_PSSUQ_UI_SUPPORT = 100 / 70;
+const MULTIPLIER_PSSUQ_UI_SUPPORT = 1.43;
+// MULTIPLIER_PSSUQ_UI = 100 / 30;
+const MULTIPLIER_PSSUQ_UI = 3.33;
 // max value of answer in sus test is 5, MAX_VALUE = 5 - 1;
 const MAX_VALUE_SUS = 4;
-// max value of answer in pssuq test is 10, MAX_VALUE = 10 - 1;
-const MAX_VALUE_PSSUQ = 9;
+
+const SummaElements = (items, start = 0, end, key) => {
+  let summa = 0;
+  if (key) {
+    for (let i = start; i < end; i++) summa += items[i][key];
+  } else {
+    for (let i = start; i < end; i++) summa += items[i];
+  }
+  return summa;
+};
+
+const SummaElementsByIndex = (items, key, ...args) => {
+  let summa = 0;
+  if (key) {
+    for (let i = 2, k; i < args.length; i++, k = args[i])
+      k > 0 ? (summa += items[k][key]) : (summa += 8 - items[k][key]);
+  } else {
+    for (let i = 2, k; i < args.length; i++, k = args[i])
+      k > 0 ? (summa += items[k]) : (summa += 8 - items[k]);
+  }
+  return summa;
+};
 
 export const SusTest = async (answers) => {
-	const value = (
-		(answers.reduce((summa, item) => summa + item.answer, -answers.length) * 100) /
-		(MAX_VALUE_SUS * answers.length)
-	).toFixed(0);
-	const type = value >= 90 ? 'A1' : value >= 60 ? 'A2' : value >= 40 ? 'A3' : 'A4';
-	return { value, type };
+  const value = (
+    (answers.reduce((summa, item) => summa + item.answer, -answers.length) *
+      100) /
+    (MAX_VALUE_SUS * answers.length)
+  ).toFixed(0);
+  const type =
+    value >= 90 ? "A1" : value >= 60 ? "A2" : value >= 40 ? "A3" : "A4";
+  return { value, type };
 };
 
 export const PssuqTest = async (answers) => {
-	const value = (
-		(answers.reduce((summa, item) => summa + item.answer, -answers.length) * 100) /
-		(MAX_VALUE_PSSUQ * answers.length)
-	).toFixed(0);
-	const type = value >= 90 ? 'A1' : value >= 60 ? 'A2' : value >= 40 ? 'A3' : 'A4';
-	return { value, type };
+  const value = (
+    answers.reduce((summa, item) => summa + item.answer, 0) *
+    MULTIPLIER_PSSUQ_COMMON
+  ).toFixed(0);
+  const usefulness =
+    SummaElements(answers, 0, 8, "answer") * MULTIPLIER_PSSUQ_USEFULNESS;
+  const qualityInfoSupport =
+    SummaElements(answers, 8, 15, "answer") * MULTIPLIER_PSSUQ_UI_SUPPORT;
+  const qualityUI =
+    SummaElements(answers, 15, 18, "answer") * MULTIPLIER_PSSUQ_UI;
+  return { value, usefulness, qualityInfoSupport, qualityUI };
 };
 
 export const MdtTest = async (results) => {
-	let plus, minus;
-	let marks = results.map((item) => item.mark);
-	plus = marks.filter((item) => item == true).length;
-	minus = marks.filter((item) => item == false).length;
-	const length = plus + minus;
-	minus &&= minus * 100 / length;
-	plus &&= plus * 100 / length;
-	return { plus, minus };
+  let plus, minus;
+  let marks = results.map((item) => item.mark);
+  plus = marks.filter((item) => item == true).length;
+  minus = marks.filter((item) => item == false).length;
+  const length = plus + minus;
+  minus &&= (minus * 100) / length;
+  plus &&= (plus * 100) / length;
+  return { plus, minus };
+};
+
+export const AcTest = async (answers) => {
+  let AK_AC = SummaElementsByIndex(
+    answers,
+    "answer",
+    11,
+    20,
+    22,
+    31,
+    34,
+    35,
+    37
+  );
+  let BO = SummaElementsByIndex(answers, "answer", 0, 16, 18, 19, -22, 28, 30);
+  let CA = SummaElementsByIndex(answers, "answer", 1, 3, 5, 6, 9, 17, 29);
+  let TO_AC = SummaElementsByIndex(
+    answers,
+    "answer",
+    2,
+    8,
+    13,
+    14,
+    24,
+    -26,
+    -27,
+    32,
+    36
+  );
+  let SP_AC = SummaElementsByIndex(
+    answers,
+    "answer",
+    -4,
+    7,
+    10,
+    12,
+    15,
+    23,
+    25,
+    33
+  );
+  return { AK_AC, BO, CA, TO_AC, SP_AC };
 };
